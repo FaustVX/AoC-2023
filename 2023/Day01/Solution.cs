61f4bb921a1b657630112ae9b468f0bb3df73dcf
FaustVX <jodu035@gmail.com>
2023-12-01 20:42:46 +0100

    Solved P1 in 0:13:55

--- 2023/Day01/Solution.cs
+++ 2023/Day01/Solution.cs
@@ -1,12 +1,25 @@
 #nullable enable
+using System.Buffers;
+
 namespace AdventOfCode.Y2023.Day01;
 
 [ProblemName("Trebuchet?!")]
 public class Solution : ISolver //, IDisplay
 {
+    private static readonly SearchValues<char> _numbers = SearchValues.Create("0123456789");
     public object PartOne(string input)
     {
-        return 0;
+        var sum = 0;
+        foreach (var line in input.AsMemory().EnumerateLines())
+            sum += GetCode(line.Span);
+        return sum;
+
+        static int GetCode(ReadOnlySpan<char> line)
+        {
+            var first = line.IndexOfAny(_numbers);
+            var last = line.LastIndexOfAny(_numbers);
+            return (line[first] - '0') * 10 + (line[last] - '0');
+        }
     }
 
     public object PartTwo(string input)
