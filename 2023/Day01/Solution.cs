15e49ee8ccecf3656d638c69ab5bd932ac515a9f
FaustVX <jodu035@gmail.com>
2023-12-01 20:28:48 +0100

    Initial commit for Y2023D1

---
+++ 2023/Day01/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2023.Day01;
+
+[ProblemName("Trebuchet?!")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
