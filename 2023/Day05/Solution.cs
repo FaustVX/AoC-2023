65a4730fb5835bd5fbc40c202ec040a94bc69d0e
FaustVX <jodu035@gmail.com>
2023-12-05 18:24:52 +0100

    Initial commit for Y2023D5

---
+++ 2023/Day05/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2023.Day05;
+
+[ProblemInfo("If You Give A Seed A Fertilizer")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+}
