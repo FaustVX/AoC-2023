c40f15dd9d66e46464a9c0c6a4a09b2a93c6770d
FaustVX <jodu035@gmail.com>
2023-12-07 22:26:45 +0100

    Solved P2 in 0:58:31

--- 2023/Day06/Solution.cs
+++ 2023/Day06/Solution.cs
@@ -9,7 +9,7 @@ public class Solution : ISolver //, IDisplay
     {
         var infos = (stackalloc Info[Globals.IsTestInput ? 3 : 4]);
         ParseInput(input.Span, infos);
-        var product = 1;
+        var product = 1UL;
         foreach (var info in infos)
             product *= info.Count();
         return product;
@@ -17,7 +17,12 @@ public class Solution : ISolver //, IDisplay
 
     public object PartTwo(ReadOnlyMemory<char> input)
     {
-        return 0;
+        var infos = (stackalloc Info[Globals.IsTestInput ? 3 : 4]);
+        ParseInput(input.Span, infos);
+        ref var last = ref infos[^1];
+        for (var i = infos.Length - 2; i >= 0 ; i--)
+            last = last.Combine(infos[i]);
+        return last.Count();
     }
 
     static void ParseInput(ReadOnlySpan<char> input, Span<Info> infos)
@@ -38,19 +43,33 @@ public class Solution : ISolver //, IDisplay
         {
             var line = lines.Current.Slice(start, Math.Min(offset, lines.Current.Length - start));
             start += offset;
-            info = info with { Record = int.Parse(line) };
+            info = info with { Record = ulong.Parse(line) };
         }
     }
 }
 
-readonly record struct Info(int Duration, int Record)
+readonly record struct Info(int Duration, ulong Record)
 {
-    public int Count()
+    public ulong Count()
     {
-        var count = 0;
-        for (var t = 1; t < Duration; t++)
-            if ((Duration - t) * t > Record)
+        var count = 0UL;
+        for (var t = 1U; t < Duration; t++)
+            if (((ulong)(Duration - t) * t) > Record)
                 count++;
         return count;
+    }
+
+    public Info Combine(Info other)
+    {
+        var result = this;
+        var power1 = (int)Math.Pow(10, (int)Math.Log10(Duration) + 1);
+        var power2 = (ulong)Math.Pow(10, (ulong)Math.Log10(Record) + 1);
+        for (var i = 0; i < 4; i++)
+        {
+            result = new(result.Duration + (other.Duration % 10) * power1, result.Record + (other.Record % 10) * power2);
+            (power1, power2) = (power1 * 10, power2 * 10);
+            other = new(other.Duration / 10, other.Record / 10);
+        }
+        return result;
     }
 }
