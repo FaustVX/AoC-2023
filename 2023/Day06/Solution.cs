3b38128e6c52eff9c64699e2fa1cd4ff1e6b5c96
FaustVX <jodu035@gmail.com>
2023-12-07 20:41:36 +0100

    Parse input

--- 2023/Day06/Solution.cs
+++ 2023/Day06/Solution.cs
@@ -6,6 +6,8 @@ public class Solution : ISolver //, IDisplay
 {
     public object PartOne(ReadOnlyMemory<char> input)
     {
+        var infos = (stackalloc Info[Globals.IsTestInput ? 3 : 4]);
+        ParseInput(input.Span, infos);
         return 0;
     }
 
@@ -13,4 +15,28 @@ public class Solution : ISolver //, IDisplay
     {
         return 0;
     }
+
+    static void ParseInput(ReadOnlySpan<char> input, Span<Info> infos)
+    {
+        var lines = input.EnumerateLines();
+        var offset = Globals.IsTestInput ? 4 : 7;
+        var start = 10;
+        lines.MoveNext();
+        foreach (ref var info in infos)
+        {
+            var line = lines.Current.Slice(start, Math.Min(offset, lines.Current.Length - start));
+            start += offset;
+            info = info with { Time = int.Parse(line) };
+        }
+        start = 10;
+        lines.MoveNext();
+        foreach (ref var info in infos)
+        {
+            var line = lines.Current.Slice(start, Math.Min(offset, lines.Current.Length - start));
+            start += offset;
+            info = info with { Distance = int.Parse(line) };
+        }
+    }
 }
+
+readonly record struct Info(int Time, int Distance);
