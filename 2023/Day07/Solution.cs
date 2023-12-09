fd47a096983751d158fa8aaf3d1e6820582409ae
FaustVX <jodu035@gmail.com>
2023-12-09 19:48:45 +0100

    Solved P2 in 1:34:16

--- 2023/Day07/Solution.cs
+++ 2023/Day07/Solution.cs
@@ -17,7 +17,7 @@ public class Solution : ISolver //, IDisplay
             lines.MoveNext();
             hand = Hand.Parse(lines.Current);
         }
-        hands.Sort(Hand.CompareTo);
+        hands.Sort(Hand.CompareTo_P1);
 
         var score = 0;
         for (var i = 0; i < hands.Length; i++)
@@ -27,7 +27,20 @@ public class Solution : ISolver //, IDisplay
 
     public object PartTwo(ReadOnlyMemory<char> input)
     {
-        return 0;
+        var hands = (stackalloc Hand[input.Span.Count('\n')]);
+        var lines = input.Span.EnumerateLines();
+        foreach (ref var hand in hands)
+        {
+            lines.MoveNext();
+            hand = Hand.Parse(lines.Current);
+            Hand.ReplaceCards(hand.Span, Rank.J, Rank._J);
+        }
+        hands.Sort(Hand.CompareTo_P2);
+
+        var score = 0;
+        for (var i = 0; i < hands.Length; i++)
+            score += (i + 1) * hands[i].Bid;
+        return score;
     }
 }
 
@@ -36,21 +49,21 @@ readonly record struct Hand(Rank Card1, Rank Card2, Rank Card3, Rank Card4, Rank
     // see: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-12.0/inline-arrays#obtaining-instances-of-span-types-for-an-inline-array-type
     public readonly Span<Rank> Span => MemoryMarshal.CreateSpan(ref Unsafe.As<Hand, Rank>(ref Unsafe.AsRef(in this)), 5);
 
-    public readonly Type Type
+    public readonly Type Type_P1
     {
         get
         {
             var cards = (stackalloc Rank[5]);
             Span.CopyTo(cards);
             cards.Sort();
-            if ((IsFive(cards)
+
+            return IsFive(cards)
             ?? IsFour(cards)
             ?? IsFull(cards)
             ?? IsThree(cards)
             ?? IsTwo(cards)
-            ?? IsOne(cards)) is Type type)
-                return type;
-            return Type.High;
+            ?? IsOne(cards)
+            ?? Type.High;
 
             static Type? IsFive(ReadOnlySpan<Rank> cards) // XXXXX
             => IsAllTheSame(cards) ? Type.Five : null;
@@ -75,13 +88,70 @@ readonly record struct Hand(Rank Card1, Rank Card2, Rank Card3, Rank Card4, Rank
         }
     }
 
-    public static bool operator >(Hand left, Hand right)
+    public readonly Type Type_P2
     {
-        if (left.Type > right.Type)
+        get
+        {
+            var cards = (stackalloc Rank[5]);
+            Span.CopyTo(cards);
+            ReplaceCards(cards, Rank._J, MostRank(cards));
+            cards.Sort();
+
+            return IsFive(cards)
+            ?? IsFour(cards)
+            ?? IsFull(cards)
+            ?? IsThree(cards)
+            ?? IsTwo(cards)
+            ?? IsOne(cards)
+            ?? Type.High;
+
+            static Type? IsFive(ReadOnlySpan<Rank> cards) // XXXXX
+            => IsAllTheSame(cards) ? Type.Five : null;
+
+            static Type? IsFour(ReadOnlySpan<Rank> cards) // XXXX_ _XXXX
+            => IsAllTheSame(cards.Slice(0, 4)) || IsAllTheSame(cards.Slice(1, 4)) ? Type.Four : null;
+
+            static Type? IsFull(ReadOnlySpan<Rank> cards) // XXYYY XXXYY
+            => (IsAllTheSame(cards.Slice(0, 2)) && IsAllTheSame(cards.Slice(2, 3))) || (IsAllTheSame(cards.Slice(0, 3)) && IsAllTheSame(cards.Slice(3, 2))) ? Type.Full : null;
+
+            static Type? IsThree(ReadOnlySpan<Rank> cards) // XXX__ _XXX_ __XXX
+            => IsAllTheSame(cards.Slice(0, 3)) || IsAllTheSame(cards.Slice(1, 3)) || IsAllTheSame(cards.Slice(2, 3)) ? Type.Three : null;
+
+            static Type? IsTwo(ReadOnlySpan<Rank> cards) // _XXYY XX_YY XXYY_
+            => (IsAllTheSame(cards.Slice(1, 2)) && IsAllTheSame(cards.Slice(3, 2))) || (IsAllTheSame(cards.Slice(0, 2)) && IsAllTheSame(cards.Slice(3, 2))) || (IsAllTheSame(cards.Slice(0, 2)) && IsAllTheSame(cards.Slice(2, 2))) ? Type.Two : null;
+
+            static Type? IsOne(ReadOnlySpan<Rank> cards)
+            => IsAllTheSame(cards.Slice(0, 2)) || IsAllTheSame(cards.Slice(1, 2)) || IsAllTheSame(cards.Slice(2, 2)) || IsAllTheSame(cards.Slice(3, 2)) ? Type.One : null;
+
+            static bool IsAllTheSame(ReadOnlySpan<Rank> cards)
+            => cards[0] == cards[^1];
+
+            static Rank MostRank(ReadOnlySpan<Rank> cards)
+            {
+                var ranks = (stackalloc Rank[12] { Rank.A, Rank.K, Rank.Q, Rank.T, Rank._9, Rank._8, Rank._7, Rank._6, Rank._5, Rank._4, Rank._3, Rank._2 });
+                var most = (rank: default(Rank), qty: 0);
+                foreach (var rank in ranks)
+                    if (cards.Count(rank) is var qty && qty > most.qty)
+                        most = (rank, qty);
+                return most.rank;
+            }
+        }
+    }
+
+    public static void ReplaceCards(Span<Rank> cards, Rank replaced, Rank by)
+    {
+        foreach (ref var c in cards)
+            if (c.Equals(replaced))
+                c = by;
+    }
+
+    private readonly bool MoreThan_P1(Hand right)
+    {
+        if (Type_P1 > right.Type_P1)
             return true;
-        if (left.Type < right.Type)
+        if (Type_P1 < right.Type_P1)
             return false;
-        return CompareHands(left.Span, right.Span);
+        return CompareHands(Span, right.Span);
 
         static bool CompareHands(ReadOnlySpan<Rank> left, ReadOnlySpan<Rank> right)
         {
@@ -95,15 +165,40 @@ readonly record struct Hand(Rank Card1, Rank Card2, Rank Card3, Rank Card4, Rank
         }
     }
 
-    [Obsolete("See '>' operator", error: true)]
-    public static bool operator <(Hand left, Hand right)
-    => throw new NotImplementedException("See '>' operator");
-
-    public static int CompareTo(Hand left, Hand right)
+    public static int CompareTo_P1(Hand left, Hand right)
     {
-        if (left > right)
+        if (left.MoreThan_P1(right))
             return 1;
-        if (right > left)
+        if (right.MoreThan_P1(left))
+            return -1;
+        return 0;
+    }
+
+    private readonly bool MoreThan_P2(Hand right)
+    {
+        if (Type_P2 > right.Type_P2)
+            return true;
+        if (Type_P2 < right.Type_P2)
+            return false;
+        return CompareHands(Span, right.Span);
+
+        static bool CompareHands(ReadOnlySpan<Rank> left, ReadOnlySpan<Rank> right)
+        {
+            if (left[0] > right[0])
+                return true;
+            if (left[0] < right[0])
+                return false;
+            if (left.Length <= 1)
+                return false;
+            return CompareHands(left[1..], right[1..]);
+        }
+    }
+
+    public static int CompareTo_P2(Hand left, Hand right)
+    {
+        if (left.MoreThan_P2(right))
+            return 1;
+        if (right.MoreThan_P2(left))
             return -1;
         return 0;
     }
@@ -111,6 +206,7 @@ readonly record struct Hand(Rank Card1, Rank Card2, Rank Card3, Rank Card4, Rank
     public static Hand Parse(ReadOnlySpan<char> line)
     {
         return new(GetRank(line[0]), GetRank(line[1]), GetRank(line[2]), GetRank(line[3]), GetRank(line[4]), ushort.Parse(line[6..]));
+
         static Rank GetRank(char c)
         => c switch
         {
@@ -145,6 +241,7 @@ enum Type : ushort
 
 enum Rank : ushort
 {
+    _J = 0,
     _2 = 2,
     _3,
     _4,
