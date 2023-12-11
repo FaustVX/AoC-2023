ffa7f50126ac6270bf1f35c2adc9cdb89e96168c
FaustVX <jodu035@gmail.com>
2023-12-11 23:37:03 +0100

    Solved P2 in 0:12:33

--- 2023/Day09/Solution.cs
+++ 2023/Day09/Solution.cs
@@ -35,7 +35,29 @@ public class Solution : ISolver //, IDisplay
 
     public object PartTwo(ReadOnlyMemory<char> input)
     {
-        return 0;
+        var sum = 0L;
+        var values = (stackalloc int[Globals.IsTestInput ? 6 : 21]);
+        foreach (var line in input.Span.EnumerateLines())
+        {
+            ParseInput(line, values);
+            sum += GetPrevious(values);
+        }
+        return sum;
+
+        static int GetPrevious(ReadOnlySpan<int> values)
+        {
+            var diffs = (stackalloc int[values.Length - 1]);
+            var all0 = true;
+            for (var i = 0; i < diffs.Length; i++)
+            {
+                var diff = diffs[i] = values[i] - values[i + 1];
+                if (all0 && diff != 0)
+                    all0 = false;
+            }
+            if (!all0)
+                return values[0] + GetPrevious(diffs);
+            return values[0];
+        }
     }
 
     static void ParseInput(ReadOnlySpan<char> line, Span<int> values)
