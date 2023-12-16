e0b2b8a66a460fcb8dd912a10358582234dbcb15
FaustVX <jodu035@gmail.com>
2023-12-16 04:44:36 +0100

    Usind a Scan Line

--- 2023/Day10/Solution.cs
+++ 2023/Day10/Solution.cs
@@ -83,71 +83,72 @@ public class Solution : ISolver //, IDisplay
     public object PartTwo(ReadOnlyMemory<char> input)
     {
         var grid = ParseInput(input.Span);
-        var (start, next) = GetOrigin(grid);
-        var previous = start;
-        while (next != start)
-        {
-            Replace(ref At(grid, previous));
-            (previous, next) = (next, NextPoint(previous, next, grid));
-        }
-        Replace(ref At(grid, previous));
-        FloodFill(grid, (0, 0));
+        ReplaceLoop(grid);
         File.WriteAllText(Path.Combine("2023", "Day10", Path.ChangeExtension(Globals.InputFileName, "txt")), input.Span.ToString());
-        return Count(grid);
+        return ScanLine(grid);
 
-        static void Replace(ref char c)
-        => c = c switch
+        static void ReplaceLoop(Grid grid)
         {
-            '|' => '┃',
-            '-' => '━',
-            'L' => '┗',
-            'J' => '┛',
-            '7' => '┓',
-            'F' => '┏',
-            'S' => '+',
-            _ => throw new UnreachableException(),
-        };
+            var (start, next, pipe) = GetOrigin(grid);
+            var previous = start;
+            At(grid, start) = pipe;
+            while (next != start)
+            {
+                Replace(ref At(grid, previous));
+                (previous, next) = (next, NextPoint(previous, next, grid));
+            }
+            Replace(ref At(grid, previous));
 
-        static int Count(ROGrid grid)
+            static void Replace(ref char c)
+            => c = c switch
+            {
+                '|' => '┃',
+                '-' => '━',
+                'L' => '┗',
+                'J' => '┛',
+                '7' => '┓',
+                'F' => '┏',
+                _ => throw new UnreachableException(),
+            };
+        }
+
+        static int ScanLine(ROGrid grid)
         {
             var sum = 0;
-            foreach (var cell in grid)
-                if (cell is not ((>= '━' and <= '┛') or '+'))
-                    sum++;
+            for (var y = 0; y < grid.Height; y++)
+            {
+                var inside = false;
+                var lastCorner = '\0';
+                for (var x = 0; x < grid.Width; x++)
+                    switch (At(grid, (y, x)))
+                    {
+                        case '┃':
+                            inside ^= true; // a xor b to invert a only if b is true
+                            break;
+                        case ('┗' or '┏') and var start:
+                            lastCorner = start;
+                            break;
+                        case '┛':
+                            if (lastCorner == '┏')
+                                inside ^= true;
+                            lastCorner = '\0';
+                            break;
+                        case '┓':
+                            if (lastCorner == '┗')
+                                inside ^= true;
+                            lastCorner = '\0';
+                            break;
+                        case '━':
+                            break;
+                        case '.' or 'L' or 'J' or 'F' or '7' or '-' or '|' when inside:
+                            sum++;
+                            break;
+                    }
+            }
             return sum;
         }
 
-        static void FloodFill(Grid grid, Point point)
-        {
-            if (point is not (>= 0, >= 0) || At(grid, point) is '*')
-                return;
-            ref var at = ref At(grid, point);
-            if (at is not ((>= '━' and <= '┛') or '+'))
-                at = '+';
-
-            FloodFill(grid, NextPoint(grid, point));
-            FloodFill(grid, NextPoint(grid, point));
-            FloodFill(grid, NextPoint(grid, point));
-            FloodFill(grid, NextPoint(grid, point));
-
-            static Point NextPoint(Grid grid, Point point)
-            {
-                if (IsInGrid(Add(point, (0, 1)), (grid.Width - 1, grid.Height - 1)) && At(grid, Add(point, (0, 1))) is not ((>= '━' and <= '┛') or '+'))
-                    return Add(point, (0, 1));
-                if (IsInGrid(Add(point, (1, 0)), (grid.Width - 1, grid.Height - 1)) && At(grid, Add(point, (1, 0))) is not ((>= '━' and <= '┛') or '+'))
-                    return Add(point, (1, 0));
-                if (IsInGrid(Add(point, (0, -1)), (grid.Width - 1, grid.Height - 1)) && At(grid, Add(point, (0, -1))) is not ((>= '━' and <= '┛') or '+'))
-                    return Add(point, (0, -1));
-                if (IsInGrid(Add(point, (-1, 0)), (grid.Width - 1, grid.Height - 1)) && At(grid, Add(point, (-1, 0))) is not ((>= '━' and <= '┛') or '+'))
-                    return Add(point, (-1, 0));
-                return (-1, -1);
-
-                static bool IsInGrid(Point point, Point size)
-                => point is (>= 0, >= 0) && Offset(point, size) is (>= 0, >= 0);
-            }
-        }
-
-        static Point NextPoint(Point previous, Point current, Grid grid)
+        static Point NextPoint(Point previous, Point current, ROGrid grid)
         {
             return grid[current.row, current.column] switch
             {
@@ -165,20 +166,32 @@ public class Solution : ISolver //, IDisplay
                 'F' => Add(current, (0, 1)),
                 _ => throw new UnreachableException(),
             };
+
+            static Point Add(Point point, Point offset)
+            => (point.row + offset.row, point.column + offset.column);
         }
 
         static Point Offset(Point from, Point to)
         => (to.row - from.row, to.column - from.column);
 
-        static Point Add(Point point, Point offset)
-        => (point.row + offset.row, point.column + offset.column);
-
-        static (Point start, Point next) GetOrigin(Grid grid)
+        static (Point start, Point next, char pipe) GetOrigin(ROGrid grid)
         {
             var start = grid.IndexOf('S');
-            return (start, Next1(start, grid));
+            var next1 = Next1(start, grid);
+            var next2 = Next2(start, grid);
+            var pipe = (Offset(start, next1), Offset(start, next2)) switch
+            {
+                ((-1, 0), (0, 1)) => 'L',
+                ((1, 0),(0, -1)) => '7',
+                ((-1, 0), (0, -1)) => 'J',
+                ((0, 1), (1, 0)) => 'F',
+                ((-1, 0), (1, 0)) => '|',
+                ((0, 1), (0, -1)) => '-',
+                _ => throw new UnreachableException(),
+            };
+            return (start, next1, pipe);
 
-            static Point Next1(Point start, Grid grid)
+            static Point Next1(Point start, ROGrid grid)
             {
                 if (grid[start.row - 1, start.column] is '7' or 'F' or '|')
                     return (start.row - 1, start.column);
@@ -188,11 +201,25 @@ public class Solution : ISolver //, IDisplay
                     return (start.row + 1, start.column);
                 throw new UnreachableException();
             }
+
+            static Point Next2(Point start, ROGrid grid)
+            {
+                if (grid[start.row, start.column - 1] is 'L' or 'F' or '-')
+                    return (start.row, start.column - 1);
+                if (grid[start.row + 1, start.column] is 'J' or 'L' or '|')
+                    return (start.row + 1, start.column);
+                if (grid[start.row, start.column + 1] is 'J' or '7' or '-')
+                    return (start.row, start.column + 1);
+                throw new UnreachableException();
+            }
         }
     }
 
     static ref char At(Grid grid, Point point)
     => ref grid[point.row, point.column];
+
+    static char At(ROGrid grid, Point point)
+    => grid[point.row, point.column];
 
     static Grid ParseInput(ReadOnlySpan<char> input)
     {
@@ -206,7 +233,7 @@ public class Solution : ISolver //, IDisplay
             _ => throw new UnreachableException(),
         } + 2; // +2 for the dots around
 
-        var span = MemoryMarshal.CreateSpan(ref input.DangerousGetReference(), input.Length);
+        var span = MemoryMarshal.CreateSpan(ref input.DangerousGetReference(), input.Length); // Convert ROSpan to Span
 
         return span.AsSpan2D(size, size + 1)[.., ..^1];
     }
