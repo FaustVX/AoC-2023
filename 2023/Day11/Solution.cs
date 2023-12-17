5b252199c2e09deb95cedd82323cc87e7953f896
FaustVX <jodu035@gmail.com>
2023-12-17 17:14:17 +0100

    Moved the previous calculation to a proper method, and added `emptyLineSize` parameter

--- 2023/Day11/Solution.cs
+++ 2023/Day11/Solution.cs
@@ -18,7 +18,7 @@ public class Solution : ISolver //, IDisplay
         ParseEmptyLine(grid, ref emptyRows, static (grid, i) => grid.GetRow(i), grid.Height);
         ParseEmptyLine(grid, ref emptyCols, static (grid, i) => grid.GetColumn(i), grid.Height);
         FindGalaxies(grid, ref galaxies);
-        return CalculateTotalDistances(galaxies, emptyRows, emptyCols);
+        return CalculateTotalDistances(galaxies, emptyRows, emptyCols, 2);
 
         static void ParseEmptyLine(Grid grid, ref Span<int> span, GetLineHandler handler, int length)
         {
@@ -46,37 +46,72 @@ public class Solution : ISolver //, IDisplay
                         galaxies[idx++] = (x, y);
             galaxies = galaxies[..idx];
         }
-
-        static int CalculateTotalDistances(ReadOnlySpan<(int x, int y)> galaxies, ReadOnlySpan<int> emptyRows, ReadOnlySpan<int> emptyCols)
-        {
-            var sum = 0;
-            foreach (var (x1, y1) in galaxies)
-            {
-                galaxies = galaxies[1..];
-                foreach (var (x2, y2) in galaxies)
-                {
-                    var distance = Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
-                    foreach (var col in emptyCols)
-                        if (col >= Math.Min(x1, x2) && col <= Math.Max(x1, x2))
-                            distance++;
-                    foreach (var row in emptyRows)
-                        if (row >= Math.Min(y1, y2) && row <= Math.Max(y1, y2))
-                            distance++;
-                    sum += distance;
-                }
-            }
-            return sum;
-        }
     }
 
     public object PartTwo(ReadOnlyMemory<char> input)
     {
-        return 0;
+        var grid = ParseInput(input.Span);
+        var emptyRows = (stackalloc int[10]);
+        var emptyCols = (stackalloc int[10]);
+        var galaxies = (stackalloc (int x, int y)[500]);
+        ParseEmptyLine(grid, ref emptyRows, static (grid, i) => grid.GetRow(i), grid.Height);
+        ParseEmptyLine(grid, ref emptyCols, static (grid, i) => grid.GetColumn(i), grid.Height);
+        FindGalaxies(grid, ref galaxies);
+        return CalculateTotalDistances(galaxies, emptyRows, emptyCols, 10);
+
+        static void ParseEmptyLine(Grid grid, ref Span<int> span, GetLineHandler handler, int length)
+        {
+            var doubledIdx = 0;
+            for (var i = 0; i < length; i++)
+                if (IsEmptyLine(handler(grid, i)))
+                    span[doubledIdx++] = i;
+            span = span[..doubledIdx];
+
+            static bool IsEmptyLine(ReadOnlyRefEnumerable<char> line)
+            {
+                foreach (var c in line)
+                    if (c is not '.')
+                        return false;
+                return true;
+            }
+        }
+
+        static void FindGalaxies(Grid grid, ref Span<(int, int)> galaxies)
+        {
+            var idx = 0;
+            for (var y = 0; y < grid.Height; y++)
+                for (var x = 0; x < grid.Width; x++)
+                    if (grid[y, x] is '#')
+                        galaxies[idx++] = (x, y);
+            galaxies = galaxies[..idx];
+        }
     }
 
     private static Grid ParseInput(ReadOnlySpan<char> input)
     {
         var size = Globals.IsTestInput ? 10 : 140;
         return input.AsSpan2D(size, size + 1)[.., ..^1];
+    }
+
+    private static int CalculateTotalDistances(ReadOnlySpan<(int x, int y)> galaxies, ReadOnlySpan<int> emptyRows, ReadOnlySpan<int> emptyCols, int emptyLineSize)
+    {
+        emptyLineSize--;
+        var sum = 0;
+        foreach (var (x1, y1) in galaxies)
+        {
+            galaxies = galaxies[1..];
+            foreach (var (x2, y2) in galaxies)
+            {
+                var distance = Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+                foreach (var col in emptyCols)
+                    if (col > Math.Min(x1, x2) && col < Math.Max(x1, x2))
+                        distance += emptyLineSize;
+                foreach (var row in emptyRows)
+                    if (row > Math.Min(y1, y2) && row < Math.Max(y1, y2))
+                        distance += emptyLineSize;
+                sum = checked(sum + distance);
+            }
+        }
+        return sum;
     }
 }
